8a67496065bc60d1f4657d02af7de6acfd31393c
Taku <53074461+Taku3939@users.noreply.github.com>
2020-07-16 22:42:29 +0900

    init

---
+++ Assets/TakuUdp/Scripts/UdpReceiver.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TakuUdp
+{
+    public class UdpReceiver : MonoBehaviour
+    {
+        [SerializeField] private List<Transform> trackers = new List<Transform>();
+
+        private UdpClient _client;
+        [SerializeField] private int port;
+        private IPEndPoint _ipEndPoint;
+
+        private void Start()
+        {
+            ReceiveEvent += b =>
+            {
+                int size_d = sizeof(double);
+                byte[] x = new byte[size_d];
+                byte[] y = new byte[size_d];
+                byte[] z = new byte[size_d];
+
+                byte[] r_x = new byte[size_d];
+                byte[] r_y = new byte[size_d];
+                byte[] r_z = new byte[size_d];
+                byte[] r_w = new byte[size_d];
+
+                int index = 0;
+
+                foreach (var tracker in trackers)
+                {
+                    if (index > b.Length)
+                        return;
+                    
+                    //Position   
+                    Array.Copy(b, index, x, 0, size_d);
+                    index = index + size_d;
+                    Array.Copy(b, index, y, 0, size_d);
+                    index = index + size_d;
+                    Array.Copy(b, index, z, 0, size_d);
+                    index = index + size_d;
+                    
+                    //Quaternion
+                    Array.Copy(b, index, r_x, 0, size_d);
+                    index = index + size_d;
+                    Array.Copy(b, index, r_y, 0, size_d);
+                    index = index + size_d;
+                    Array.Copy(b, index, r_z, 0, size_d);
+                    index = index + size_d;
+                    Array.Copy(b, index, r_w, 0, size_d);
+                    index = index + size_d;
+
+                    var x_f = BitConverter.ToDouble(x, 0);
+                    var y_f = BitConverter.ToDouble(y, 0);
+                    var z_f = BitConverter.ToDouble(z, 0);
+
+                    var rxf = BitConverter.ToDouble(r_x, 0);
+                    var ryf = BitConverter.ToDouble(r_y, 0);
+                    var rzf = BitConverter.ToDouble(r_z, 0);
+                    var rwf = BitConverter.ToDouble(r_w, 0);
+                    tracker.position = new Vector3((float) x_f, (float) y_f, (float) z_f);
+                    tracker.rotation = new Quaternion((float) rxf, (float) ryf, (float) rzf, (float) rwf);
+                }
+            };
+
+            _ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+            _client = new UdpClient(_ipEndPoint);
+
+
+            //s.Result.Buffer
+            Receive();
+
+        }
+
+        async Task Receive()
+        {
+            var s = await _client.ReceiveAsync();
+            ReceiveEvent?.Invoke(s.Buffer);
+            Receive();
+        }
+
+        public event Action<byte[]> ReceiveEvent;
+
+        public void OnApplicationQuit()
+        {
+            _client.Dispose();
+        }
+    }
+}
---
+++ Assets/TakuUdp/Scripts/UdpSender.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEditor;
+using UnityEngine;
+
+namespace TakuUdp
+{
+    public class UdpSender : MonoBehaviour
+    {
+        [SerializeField] private List<Transform> trackers = new List<Transform>();
+        [SerializeField] private string ip;
+        [SerializeField] private int port;
+
+        [SerializeField] private int packet_limit = 1024;
+        private UdpClient client;
+        public bool IsConnect { get; set; }
+        
+        public void Connect()
+        {
+            client = new UdpClient();
+            client.Connect(ip, port);
+            IsConnect = true;
+        }
+
+        public void Disconnect()
+        {
+            if (client == null)
+            {
+                return;
+            }
+            client.Dispose();
+            IsConnect = false;
+        }
+
+        /// <summary>
+        /// Udp Send
+        /// </summary>
+        public void Send()
+        {
+            List<Transform> transforms = new List<Transform>();
+            trackers.ForEach(x => transforms.Add(x));
+            Send(transforms);
+        }
+        
+        public void Send(List<Transform> transforms)
+        {
+            var bits = convert(transforms);
+            client.SendAsync(bits, bits.Length);
+        }
+        
+        /// <summary>
+        /// Transformの配列をdouble型をbyte配列変換する
+        /// </summary>
+        /// <param name="transforms">ObjectのTransform</param>
+        /// <returns></returns>
+        byte[] convert(List<Transform> transforms)
+        {
+            byte[] sendFrame = new byte[1024];
+            
+            int index = 0;
+            var size_d = sizeof(double);
+            transforms.ForEach(tr =>
+            {
+                //Position
+                var pos = tr.position;
+                byte[] x = BitConverter.GetBytes((double)pos.x);
+                byte[] y = BitConverter.GetBytes((double)pos.y);
+                byte[] z = BitConverter.GetBytes((double)pos.z);
+
+                
+                //Quaternion
+                var rot = tr.rotation;
+                byte[] r_x = BitConverter.GetBytes((double)rot.x);
+                byte[] r_y = BitConverter.GetBytes((double)rot.y);
+                byte[] r_z = BitConverter.GetBytes((double)rot.z);
+                byte[] r_w = BitConverter.GetBytes((double)rot.w);
+                
+                //Position
+                Array.Copy(x, 0, sendFrame, index, size_d);
+                index += size_d;
+                Array.Copy(y, 0, sendFrame, index, size_d);
+                index += size_d; 
+                Array.Copy(z, 0, sendFrame, index, size_d);
+                index += size_d;
+                
+                //Quaternion
+                Array.Copy(r_x, 0, sendFrame, index, size_d);
+                index += size_d;
+                Array.Copy(r_y, 0, sendFrame, index, size_d);
+                index += size_d; 
+                Array.Copy(r_z, 0, sendFrame, index, size_d);
+                index += size_d;
+                Array.Copy(r_w, 0, sendFrame, index, size_d);
+                index += size_d;
+            });
+
+            return sendFrame;
+        }
+        
+        private void Update()
+        {
+            if(!IsConnect)
+                return;
+            Send();
+        }
+
+        public void OnApplicationQuit()
+        {
+            client.Dispose();
+        }
+    }
+
+    [CustomEditor(typeof(UdpSender))]
+    public class UdpSenderEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var sender = target as UdpSender;
+            
+            EditorGUI.BeginChangeCheck();
+
+            sender.IsConnect = GUILayout.Toggle(sender.IsConnect, new GUIContent("IsConnect"));
+            
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (sender.IsConnect)
+                {
+                    sender.Connect();
+                }
+                else
+                {
+                    sender.Disconnect();
+                }
+                
+            }
+        }
+    }
+}
